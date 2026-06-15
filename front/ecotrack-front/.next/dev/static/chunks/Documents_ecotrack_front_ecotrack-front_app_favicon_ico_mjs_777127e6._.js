bf30209bb66af9b14a8eae5088a684f548908b05
Chamssoudine <chsak@gmail.com>
2026-06-15 10:52:59 +0200

    Deploy simulé

--- front/ecotrack-front/.next/dev/static/chunks/Documents_ecotrack_front_ecotrack-front_app_favicon_ico_mjs_777127e6._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/9faea_next_dist_bf0e9d70._.js"
-],
-    source: "dynamic"
-});
